3cc656349d7aa5ba234bdc5f5a9073087f3cfe02
Rade58 <bajic.rade2@gmail.com>
2023-12-12 22:25:58 +0100

    generic

---
+++ qa.js
@@ -0,0 +1,5 @@
+const video =
+  'https://www.youtube.com/watch?v=zR_iuq2evXo&list=PLum3CyP95edxwLIHenKw0nMHlfvr76ZSU&index=7'
+const question = process.argv[2] || 'Zdravo!'
+
+console.log(`Your question: "${question}"\n`)
